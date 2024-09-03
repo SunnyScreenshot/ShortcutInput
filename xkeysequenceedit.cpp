3eb0c0827dd5ece466bf47804c539cfde9d9cb91
XMuli <xmulitech@gmail.com>
2024-09-03 13:37:44 +0800

    chore: init

---
+++ xkeysequenceedit.cpp
@@ -0,0 +1,73 @@
+﻿#include "xkeysequenceedit.h"
+#include <QWidget>
+#include <QMouseEvent>
+#include <QDebug>
+
+/*!
+ * \brief The XKeySequenceEdit class 设置仅接受一个快捷键组合；删除清空
+ */
+
+XKeySequenceEdit::XKeySequenceEdit(QWidget *parent)
+    : QKeySequenceEdit(parent)
+{
+    initUI();
+}
+
+XKeySequenceEdit::XKeySequenceEdit(const QKeySequence &keySequence, QWidget *parent)
+    : QKeySequenceEdit(keySequence, parent)
+{
+    initUI();
+}
+
+void XKeySequenceEdit::initUI()
+{
+    //setWindowFlags(Qt::FramelessWindowHint | Qt::Popup);
+}
+
+//void XKeySequenceEdit::test()
+//{
+//	connect(this, &XKeySequenceEdit::keySequenceChanged, this, &XKeySequenceEdit::onKeySequenceChanged);
+
+//	connect(this, &XKeySequenceEdit::editingFinished, this, &XKeySequenceEdit::onEditingFinished);
+//}
+
+//void XKeySequenceEdit::onEditingFinished()
+//{
+//	qInfo() << "editingFinished    ------------>" << keySequence() << keySequence().count();
+//}
+
+//void XKeySequenceEdit::onKeySequenceChanged(const QKeySequence & keySequence)
+//{
+//	qInfo() << "keySequenceChanged ------------>" << keySequence << keySequence.count();
+//}
+
+// Ref: https://jishurizhi.com/p-74.html
+void XKeySequenceEdit::keyPressEvent(QKeyEvent *event)
+{
+    QKeySequenceEdit::keyPressEvent(event);
+
+    QKeySequence keySeq = keySequence();
+    qInfo() << "keySeq:" << keySeq << "keySeq.count():" << keySeq.count();
+
+    // Fix: 按下相同快捷键编辑框为空白
+    if ( keySeq.count() <= 0)
+        return;
+
+    int key = keySeq[0];
+    qDebug()<<"keySeq[0]:"<<keySeq[0];
+    if (key == Qt::Key_Backspace || key == Qt::Key_Delete)
+        key = 0;
+
+    setKeySequence(key);
+    emit sigKeySeqChanged(key); // 要发射对应信号
+}
+
+//void XKeySequenceEdit::mousePressEvent(QMouseEvent* event)
+//{
+//    int a = 0;
+//    if (event->button() != Qt::LeftButton)
+//        return;
+//
+//    if (!geometry().contains(mapFromGlobal(QCursor::pos())))
+//        clearFocus();
+//}
---
+++ xkeysequenceedit.h
@@ -0,0 +1,34 @@
+﻿#ifndef XKEYSEQUENCEEDIT_H
+#define XKEYSEQUENCEEDIT_H
+
+#include "xwidgets_global.h"
+#include <QKeySequenceEdit>
+
+class XWIDGETS_EXPORT XKeySequenceEdit : public QKeySequenceEdit
+{
+    Q_OBJECT
+public:
+    explicit XKeySequenceEdit(QWidget *parent = nullptr);
+    explicit XKeySequenceEdit(const QKeySequence &keySequence, QWidget *parent = nullptr);
+    virtual ~XKeySequenceEdit() = default;
+
+private:
+    void initUI();
+
+//	void test();
+
+signals:
+    void sigKeySeqChanged(const QKeySequence &keySequence);
+
+//public slots:
+    //void onEditingFinished();
+    //void onKeySequenceChanged(const QKeySequence &keySequence);
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+    //void mousePressEvent(QMouseEvent* event) override;
+};
+
+
+
+#endif // XKEYSEQUENCEEDIT_H
